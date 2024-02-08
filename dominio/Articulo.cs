4c286f66f59cf0c8594b1f5786717c2e23ffd486
valensarnari <valentinsarnari@gmail.com>
2024-02-08 10:39:00 -0300

    Nueva clase de acceso a DB y modelado de clases de dominio

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -8,5 +8,16 @@ namespace dominio
 {
     public class Articulo
     {
+        // atributos y propiedades
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public string ImagenUrl { get; set; }
+        public float Precio { get; set; }
+
+        // constructor y metodos
     }
 }
